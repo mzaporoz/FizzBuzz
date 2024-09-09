d0911f02eae66ee392f6fc79932e1e594f3b6c90
mzaporoz <michelle.zaporozhets@mail.utoronto.ca>
2024-09-09 14:25:27 -0400

    changed comment

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,6 +1,7 @@
 public class Multiples {
     public static void main(String[] args) {
-        int c = 0; //start count
+        //prints how many multiples of 3 or 5 there are below 1000
+        int c = 0; //number count
 
         for (int i = 0; i < 1000; i++) {
             boolean multipleOf3 = i % 3 == 0;
